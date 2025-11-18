c05142060aee029e8602256fbfbe9d2222368b76
DESKTOP-90KR9TR\9999 <nguyenvancuong.tm@gmail.com>
2025-11-18 21:17:03 +0700

    update code

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -157,10 +157,10 @@ export default async function RootLayout(props: {
         <link rel="image_src" href={ogImage} />
         <link rel="canonical" href={siteUrl} />
       </head>
-      <Script
+      {/* <Script
         src={`https://www.27biggroup.com/librarywedding/functionWedding/library27biggroup.js`}
         strategy="afterInteractive"
-      />
+      /> */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX`}
         strategy="afterInteractive"
